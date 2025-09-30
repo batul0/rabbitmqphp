d4699b5c5eec8aa9e2cee600749d0623ebe88cf0
Craig <cak42@njit.edu>
2025-09-29 21:11:52 -0400

    Finished register.php and register.html, also updated testRabbitMQClient.php to inlcuded necessary functions for later. Added a nav bar for easier testing.

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -4,7 +4,57 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+function login($username, $password){
+
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+
+    $request = array();
+    $request['type'] = "login";
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $request['message'] = "HI";
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+
+    echo "client received response: ".PHP_EOL;
+    print_r($response);
+    echo "\n\n";
+
+}
+
+function register($username, $password) {
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    
+    $request = array();
+    $request['type'] = "register";
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $response = $client->send_request($request);
+    return $response;
+}
+
+function logout($token) {
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+    $request = array();
+    $request['type'] = "logout";
+    $request['token'] = $token;
+    $response = $client->send_request($request);
+    return $response;
+}
+
+function validate($token) {
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    
+    $request = array();
+    $request['type'] = "validate";
+    $request['token'] = $token;
+    $response = $client->send_request($request);
+    return $response;
+}
+
+/*$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
 
 $request = array();
@@ -18,6 +68,5 @@ $response = $client->send_request($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
-
+*/
 echo $argv[0]." END".PHP_EOL;
-
